3ff46e3cdc17e1d2e50b2a2572ddbcf5155d4081
arseny <arsenid2003@gmail.com>
2025-05-25 20:31:35 +0500

    init project with tasks

---
+++ src/task1.ts
@@ -0,0 +1,7 @@
+
+const totalPrice = ({ price, discount, isInstallment, months }) => {
+    // Your code here...
+};
+
+const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
+console.log(price); // 6250
